fc90a64d4496be19d0a0e01cd239430badec28ad
Sanjana Bhagavatula <102685292+sanjanabhagavatula@users.noreply.github.com>
2025-05-27 17:54:44 -0400

    updated image sizing and fade in

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let images;
 let image = document.getElementById("image");
+let timeout = 10000;
+let fadeInTime = 2000;
+
 
 let fetchImages = async () => {
     const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzaAupbvxvxC5RLT6772_gsyFa0tfF3wFUPS1b8DbAqA7HsYjxesDGQlMa4jpIw0_1UMw/exec'; // Replace with your /exec URL
@@ -9,24 +12,21 @@ let fetchImages = async () => {
 }
 
 let fadeIn = () => {
-	console.log("fading in image")
 	const idx = Math.floor(Math.random() * images.length)
-	console.log(image.style.opacity)
 	image.src = images[idx].url;
 	image.className = "in"
-	setTimeout(fadeOut, 5000);
+	setTimeout(fadeOut, timeout);
 }
 
 let fadeOut = () => {
-	console.log("fading out image");
 	image.className = 'out'
-	console.log(image.style.opacity)
-	fadeIn();
+	setTimeout(fadeIn, fadeInTime);
 }
 
 let main = async () => {
 	try {
 		images = await fetchImages();
+		setInterval(async () => {images = await fetchImages()}, 2 * 60 * 1000)
 		fadeIn();
 	} catch (e) {
 		console.log(e)
